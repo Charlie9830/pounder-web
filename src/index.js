d8ee618b4a8284cb9999af45164a65d1edfd5c27
Charlie Hall <charlie.hall@ozemail.com.au>
2017-11-05 17:24:13 +1100

    Visual Improvements

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import VisibleApp from './components/App';
 import { Provider } from 'react-redux';
-import { appStore } from 'pounder-redux';
+import { setupBackend ,appStore } from 'pounder-redux';
 import registerServiceWorker from './registerServiceWorker';
 
+setupBackend("development", "mobile");
 
 ReactDOM.render(<Provider store={appStore}><VisibleApp /></Provider>, document.getElementById('root'));
 registerServiceWorker();

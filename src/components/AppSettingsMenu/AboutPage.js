28d2b605db2b5d56d26424d34a6a4af72698b45d
Charlie Hall <charlie.hall@ozemail.com.au>
2018-08-13 14:46:51 +1000

    Added Dependency version to About Page.

--- src/components/AppSettingsMenu/AboutPage.js
+++ src/components/AppSettingsMenu/AboutPage.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import CenteringContainer from '../../containers/CenteringContainer';
+import HorizontalCenteringContainer from '../../containers/HorizontalCenteringContainer';
 import Button from '../Button';
 import '../../assets/css/AppSettingsMenu/AboutPage.css';
 import AppIcon from '../../assets/icons/Handball-Icon-Desktop-Draft.svg'
+let dependencyVersions = [
+    { name: "pounder-redux", value: require('pounder-redux/package.json').version },
+    { name: "pounder-firebase", value: require('pounder-firebase/package.json').version },
+    { name: "pounder-stores", value: require('pounder-stores/package.json').version },
+    { name: "pounder-utilities", value: require('pounder-utilities/package.json').version },
+    { name: "pounder-dexie", value: require('pounder-dexie/package.json').version },
+    { name: "firestore-batch-paginator", value: require('firestore-batch-paginator/package.json').version },
+    
+]
 
 // eslint-disable-next-line
 const appVersion = HANDBALL_VERSION;
 
 class AboutPage extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            showDependencyVersionInfo: false,
+        }
+        // Method Bindings.
+        this.getDependencyJSX = this.getDependencyJSX.bind(this);
+    }
     render() {
+        var dependencyJSX = this.getDependencyJSX();
         return (
             <div className="AboutPageContainer">
                 <CenteringContainer>
                     <div className="AboutPageAppInfoContainer">
-                        <img className="AboutPageAppIcon" src={AppIcon} />
+                        <img className="AboutPageAppIcon" src={AppIcon}
+                        onDoubleClick={() => {this.setState({showDependencyVersionInfo: true})}} />
                         <div className="AboutPageAppTitle"> Handball </div>
                         <div className="AboutPageVersionNumber"> Version {appVersion} </div>
                         <div className="AboutPageAuthor"> Charlie Hall </div>
                         
                         <div className="AboutPageDivider"/>
+                        {dependencyJSX}
 
                     </div>
 
@@ -36,6 +57,24 @@ class AboutPage extends React.Component {
                 </CenteringContainer>
             </div>
         )
+    }
+
+    getDependencyJSX() {
+        if (this.state.showDependencyVersionInfo) {
+            var jsx = dependencyVersions.map((item,index) => {
+                return (
+                    <HorizontalCenteringContainer key={index}>
+                        <div className="AboutPageSystemValue"> {item.name} : {item.value} </div>
+                    </HorizontalCenteringContainer>
+                )
+            })
+    
+            return (
+                <div className="AboutPageSystemValueContainer">
+                    {jsx}
+                </div>
+            )
+        }
     }
 }
 

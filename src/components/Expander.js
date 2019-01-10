bd09bd9dbef168f38ba0ffd425a808efc24695a2
Charlie Hall <charlie.hall@ozemail.com.au>
2019-01-11 01:17:58 +1100

    Expander

--- src/components/Expander.js
+++ src/components/Expander.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Popover, Toolbar, IconButton } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
+const styles = {
+    paper: {
+        maxWidth: '100vw',
+        maxHeight: '100vh',
+        top: 0,
+        left: 0,
+    }
+}
+
 let gridStyle = {
-    width: '85vw',
-    height: '90vh',
+    width: '100vw',
+    height: '100vh',
     display: 'grid',
     gridTemplateRows: '[Toolbar]56px [Children]1fr',
     overflowY: 'hidden'
 }
 
 const Expander = (props) => {
+    let { classes } = props;
+
     return (
         <Popover
+        classes={{ paper: classes.paper}}
         open={props.open}
+        marginThreshold={0}
         anchorEl={props.anchorEl}
         anchorOrigin={{ vertical: 'center', horizontal: 'center'}}
         transformOrigin={{ vertical: 'center', horizontal: 'center'}}>
@@ -37,4 +51,4 @@ const Expander = (props) => {
 };
 
 
-export default Expander;
+export default withStyles(styles)(Expander);

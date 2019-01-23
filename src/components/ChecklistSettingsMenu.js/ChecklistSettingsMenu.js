67393edc0afb03ffcfaf123348cfdd03a63d1310
Charlie Hall <charlie.hall@ozemail.com.au>
2019-01-23 23:17:52 +1100

    Fix: Added paper layout to ChecklistSettingsMenu.

--- src/components/ChecklistSettingsMenu.js/ChecklistSettingsMenu.js
+++ src/components/ChecklistSettingsMenu.js/ChecklistSettingsMenu.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { updateChecklistSettingsAsync, manuallyRenewChecklistAsync, closeChecklistSettings } from 'handball-libs/libs/pounder-redux/action-creators';
 import {
     Toolbar, IconButton, Typography, FormControlLabel, Switch, Button, List, ListItem, ListItemIcon,
-    Input, FormControl, InputLabel, InputAdornment
+    Input, FormControl, InputLabel, InputAdornment, Paper
 } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import RefreshIcon from '@material-ui/icons/Refresh';
@@ -55,64 +55,67 @@ class ChecklistSettingsMenu extends Component {
                         <ArrowBackIcon/>
                     </IconButton>
                 </Toolbar>
-                <div style={contentGrid}>
-                    <div style={{gridArea: 'Information', placeSelf: 'center flex-start'}}>
-                        <Typography color="textSecondary">
-                            Checklists allow you to set a date and interval in which completed tasks in this list will be automatically renewed.
+                <Paper
+                style={{padding: '8px', margin: '8px'}}>
+                    <div style={contentGrid}>
+                        <div style={{ gridArea: 'Information', placeSelf: 'center flex-start' }}>
+                            <Typography color="textSecondary">
+                                Checklists allow you to set a date and interval in which completed tasks in this list will be automatically renewed.
                         </Typography>
-                    </div>
+                        </div>
 
-                    <div style={{gridArea: 'Enable', placeSelf: 'center flex-start'}}>
-                        <FormControlLabel
-                        label={"Enable"}
-                        control={<Switch
-                            checked={settings.isChecklist}
-                            onChange={(e) => { this.handleEnabledChange(e.target.checked) }} />}
-                        />
-                    </div>
+                        <div style={{ gridArea: 'Enable', placeSelf: 'center flex-start' }}>
+                            <FormControlLabel
+                                label={"Enable"}
+                                control={<Switch
+                                    checked={settings.isChecklist}
+                                    onChange={(e) => { this.handleEnabledChange(e.target.checked) }} />}
+                            />
+                        </div>
 
-                    <div style={{gridArea: 'Renew', placeSelf: 'center flex-end'}}>
-                        <Button
-                        onClick={() => { this.props.dispatch(manuallyRenewChecklistAsync(this.props.openChecklistSettingsId))}}
-                        variant="outlined"
-                        color='secondary'
-                        disabled={disabled}>
-                            Renew now
+                        <div style={{ gridArea: 'Renew', placeSelf: 'center flex-end' }}>
+                            <Button
+                                onClick={() => { this.props.dispatch(manuallyRenewChecklistAsync(this.props.openChecklistSettingsId)) }}
+                                variant="outlined"
+                                color='secondary'
+                                disabled={disabled}>
+                                Renew now
                         </Button>
+                        </div>
+
+                        <div style={{ gridArea: 'SettingsList', placeSelf: 'flex-start' }}>
+                            <List>
+                                <DateInputListItem
+                                    disabled={disabled}
+                                    placeholder="Set renew date"
+                                    value={settings.initialStartDate}
+                                    onChange={this.handleDateChange}
+                                    disablePast={true}
+                                    autoOk={true}
+                                    clearable={true} />
+
+                                <ListItem
+                                    disabled={disabled}>
+                                    <ListItemIcon>
+                                        <RefreshIcon />
+                                    </ListItemIcon>
+
+                                    <FormControl>
+                                        <InputLabel> Then renew every </InputLabel>
+                                        <Input
+                                            type='number'
+                                            error={!this.state.isRenewIntervalValid}
+                                            defaultValue={settings.renewInterval}
+                                            onChange={(e) => { this.validateRenewInterval(e.target.value) }}
+                                            onBlur={(e) => { this.handleRenewIntervalBlur(e.target.value) }}
+                                            endAdornment={<InputAdornment position='end'> days </InputAdornment>} />
+                                    </FormControl>
+
+                                </ListItem>
+                            </List>
+                        </div>
                     </div>
-
-                    <div style={{gridArea: 'SettingsList', placeSelf: 'flex-start'}}>
-                        <List>
-                            <DateInputListItem
-                            disabled={disabled}
-                            placeholder="Set renew date"
-                            value={settings.initialStartDate}
-                            onChange={this.handleDateChange}
-                            disablePast={true}
-                            autoOk={true}
-                            clearable={true}/>
-
-                            <ListItem
-                            disabled={disabled}>
-                                <ListItemIcon>
-                                    <RefreshIcon/>
-                                </ListItemIcon>
-
-                                <FormControl>
-                                    <InputLabel> Then renew every </InputLabel>
-                                    <Input 
-                                    type='number'
-                                    error={!this.state.isRenewIntervalValid}
-                                    defaultValue={settings.renewInterval}
-                                    onChange={(e) => { this.validateRenewInterval(e.target.value) }}
-                                    onBlur={(e) => { this.handleRenewIntervalBlur(e.target.value) }}
-                                    endAdornment={<InputAdornment position='end'> days </InputAdornment>}/>
-                                </FormControl>
-                                
-                            </ListItem>
-                        </List>
-                    </div>
-                </div>
+                </Paper>
             </FullScreenView>
         );
     }

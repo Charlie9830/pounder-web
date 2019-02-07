c54cce9cd7acceaa0eca56387fbb9ff86fe6769b
Charlie Hall <charlie.hall@ozemail.com.au>
2019-02-07 14:32:00 +1100

    Fix: ProjectSelector unseenCommentIndicator Color

--- src/components/ProjectListItem/UnseenCommentsChit.js
+++ src/components/ProjectListItem/UnseenCommentsChit.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CommentIcon from '@material-ui/icons/Comment';
 
 const UnseenCommentsIndicator = (props) => {
-    return <CommentIcon fontSize="small"/>
+    return <CommentIcon color="action" fontSize="small"/>
 }
 
 export default UnseenCommentsIndicator;
